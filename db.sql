488bf672e91fca00d87124849f89536a1a4786b0
Per Fimreite <per.fimreite@gmail.com>
2026-03-27 19:28:35 +0100

    Iterasjon 3 db.sql

--- db.sql
+++ db.sql
@@ -7,14 +7,39 @@ CREATE TABLE ansatt (
     brukernavn     VARCHAR(30) UNIQUE NOT NULL,
     fornavn        VARCHAR(30) NOT NULL,
     etternavn      VARCHAR(30) NOT NULL,
-    ansettelsedato DATE        NOT NULL,
+    ansettelsedato DATE        NOT NULL DEFAULT CURRENT_DATE,
     stilling       VARCHAR(30) NOT NULL,
-    manedslonn     INTEGER     NOT NULL
+    manedslonn     INTEGER     NOT NULL,
+    avdeling_id    INTEGER NOT NULL REFERENCES avdeling ON DELETE RESTRICT
 );
 
-INSERT INTO ansatt (brukernavn, fornavn, etternavn, ansettelsedato, stilling, manedslonn)
-VALUES
-    ('ola12', 'Ola', 'Nordmann', CURRENT_DATE, 'Utvikler', 55000),
-    ('kari34', 'Kari', 'Nordmann', CURRENT_DATE, 'Sjef', 70000);
+CREATE TABLE avdeling (
+    id   SERIAL PRIMARY KEY,
+    navn VARCHAR(30) UNIQUE NOT NULL
+);
 
-SELECT * FROM ansatt
+ALTER TABLE avdeling ADD COLUMN sjef_id INTEGER UNIQUE;
+
+ALTER TABLE avdeling
+    ADD CONSTRAINT avdeling_sjef_fk
+        FOREIGN KEY (sjef_id)
+            REFERENCES ansatt(id)
+            ON DELETE RESTRICT;
+
+INSERT INTO avdeling (navn) VALUES
+    ('IT'),
+    ('Salg');
+
+INSERT INTO ansatt (brukernavn, fornavn, etternavn, stilling, manedslonn, avdeling_id) VALUES
+    ('lhp', 'Lars', 'Hansen', 'Avdelingsleder IT', 85000, 1),
+    ('mab', 'Mari', 'Berg', 'Systemutvikler', 62000, 1),
+    ('kai', 'Kari', 'Iversen', 'Testleder', 58000, 1),
+    ('sjo', 'Svein', 'Johansen', 'Salgsdirektør', 90000, 2),
+    ('ema', 'Erik', 'Martinsen', 'Salgskonsulent', 55000, 2);
+
+UPDATE avdeling SET sjef_id = (SELECT id FROM ansatt WHERE brukernavn = 'lhp') WHERE id = 1;
+UPDATE avdeling SET sjef_id = (SELECT id FROM ansatt WHERE brukernavn = 'sjo') WHERE id = 2;
+
+SELECT * FROM ansatt;
+
+SELECT * FROM avdeling;
